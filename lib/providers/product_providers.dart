2a4b9cbcb8ae1f3a571a8535b6116216a2ff4ddf
nishtha-dev <nishthasaraswat15@gmail.com>
2022-06-21 22:20:44 +0530

    added favouriteItems getter

--- lib/providers/product_providers.dart
+++ lib/providers/product_providers.dart
@@ -6,13 +6,32 @@ import 'package:shop_app/providers/products.dart';
 class Products with ChangeNotifier {
   List<Product> _items = dummyProducts;
 
+  //bool _showFavoriteOnly = false;
+
   List<Product> get items {
+    // if (_showFavoriteOnly) {
+    //   return _items.where((prodItem) => prodItem.isFavourate).toList();
+    // }
     return [..._items];
+  }
+
+  List<Product> get favoriteItems {
+    return _items.where((prodItem) => prodItem.isFavourate).toList();
   }
 
   Product findById(String id) {
     return _items.firstWhere((element) => element.id == id);
   }
+
+  // void showfavorites() {
+  //   _showFavoriteOnly = true;
+  //   notifyListeners();
+  // }
+
+  // void showAll() {
+  //   _showFavoriteOnly = false;
+  //   notifyListeners();
+  // }
 
   void addProducts() {
     notifyListeners();
