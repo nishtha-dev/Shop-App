5113a70ff9eedd7047aed02a4e1c1a59c1a2103f
nishtha-dev <nishthasaraswat15@gmail.com>
2022-06-23 01:30:25 +0530

    Created CartItem widget

---
+++ lib/widgets/cart_item.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/providers/cart.dart';
+
+class CartItem extends StatelessWidget {
+  final String title;
+  final int quantity;
+  final double price;
+  final String id;
+  final String productId;
+
+  CartItem(
+      {required this.id,
+      required this.price,
+      required this.quantity,
+      required this.title,
+      required this.productId});
+
+  @override
+  Widget build(BuildContext context) {
+    return Dismissible(
+      key: ValueKey(id),
+      background: Container(
+        color: Theme.of(context).errorColor,
+        margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 8),
+      ),
+      direction: DismissDirection.endToStart,
+      onDismissed: (direction) {
+        Provider.of<Cart>(context, listen: false).removeItem(productId);
+      },
+      child: Card(
+        margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 8),
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: ListTile(
+            leading: CircleAvatar(
+              child: Padding(
+                  padding: const EdgeInsets.all(5),
+                  child: FittedBox(child: Text('\$$price'))),
+            ),
+            title: Text(title),
+            subtitle: Text('Total: \$${price * quantity} '),
+            trailing: Text('$quantity x'),
+          ),
+        ),
+      ),
+    );
+  }
+}
